46ab26999347454ba38ab76d009be7a166a4a772
Haresh <lakhwaniharesh134@gmail.com>
2024-05-19 22:56:09 +0530

    Off;ine support stared service workers

---
+++ offline-support/learning.js
@@ -0,0 +1,16 @@
+/**
+ *  UNDERSTANING SERVICE WORKERS 
+ * 
+ *  Peice of js code runs in other thread rether then in main thread
+ * 
+ *  Seprate thread it does not block main thread
+ * 
+ *  Provided by browsers 
+ * 
+ *  Using this service workers we can create offline experience 
+ * 
+ *  INTERSEPT NETWORK REQUEST
+ *  
+ *  BEHAVE AS PROXY SERVER
+ * 
+ */
